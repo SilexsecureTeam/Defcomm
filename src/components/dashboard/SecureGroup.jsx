ff1aeb6efe0705c539e6729b50838980b8d3226d
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-02-20 14:44:29 +0100

    Update SecureGroup.jsx

--- src/components/dashboard/SecureGroup.jsx
+++ src/components/dashboard/SecureGroup.jsx
@@ -2,12 +2,12 @@ import { RiLockFill } from "react-icons/ri";
 
 function SecureGroup() {
   return (
-    <div className="bg-[#1a2b12] p-4 rounded-lg mt-4 flex space-x-4 items-center">
+    <div className="bg-[#1a2b12] p-4 rounded-lg mt-4 flex flex-col space-y-4 justify-center">
       <h2 className="text-lg font-semibold flex items-center space-x-2">
         <RiLockFill size={20} className="text-green-400" />
         <span>Secure Group</span>
       </h2>
-      <div className="flex space-x-2">
+      <div className="flex space-x-2 overflow-x-auto">
         {[...Array(5)].map((_, index) => (
           <div key={index} className="w-12 h-12 bg-gray-700 rounded-full"></div>
         ))}
