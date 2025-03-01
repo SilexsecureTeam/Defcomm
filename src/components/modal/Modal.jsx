c1ae651360fc29de51f85a82fc00ce4b48b8a249
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-03-01 15:01:47 +0100

    Update Modal.jsx

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -8,7 +8,7 @@ const Modal = ({ isOpen, closeModal, children }) => {
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: 50 }}
-        className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+        className="w-screen h-screen fixed inset-0 flex items-center justify-center bg-black/50 z-50">
         <div className="relative bg-white flex flex-col rounded-lg shadow-lg w-max">
           <button onClick={closeModal} className="absolute top-3 right-3 text-red-500 font-bold text-lg float-right p-5">✖</button>
           <section className="overflow-y-auto">
