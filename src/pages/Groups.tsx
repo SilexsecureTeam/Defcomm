4e7e5d76be4b97db110318d18f7e901d0f42ecb7
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-03-10 01:55:15 +0100

    Update Groups.tsx

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -79,7 +79,7 @@ const Groups = () => {
                 ) : invitations?.length ? (
                     <ul className="space-y-4">
                         {invitations.map((inv) => (
-                            <li key={inv.id} className="p-4 rounded-lg flex justify-between items-center shadow-md gap-1">
+                            <li key={inv.id} className="p-4 rounded-lg flex flex-col md:flex-row justify-between items-center shadow-md gap-1">
                                 <span className="text-sm md:text-lg font-medium">{inv.group_name}</span>
                                 <div className="flex gap-2">
                                     <button
