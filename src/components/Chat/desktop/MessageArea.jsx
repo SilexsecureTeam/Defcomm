8986ad8cf2955cbb9466d47924e7effae584d559
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-04-16 10:01:00 +0100

    Update MessageArea.jsx

--- src/components/Chat/desktop/MessageArea.jsx
+++ src/components/Chat/desktop/MessageArea.jsx
@@ -2,7 +2,7 @@ import ChatInterface from "./ChatInterface";
 
 export default function MessageArea() {
     return (
-      <div className="flex-1 bg-gray-800 overflow-y-auto">
+      <div className="flex-1 bg-[#1B1C18] overflow-y-auto">
         <ChatInterface />
       </div>
     );
