196b54038f888c8f3d1e40c089c3b1e2d4f71015
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-05-21 18:17:00 +0100

    Update MeetingContext.jsx

--- src/context/MeetingContext.jsx
+++ src/context/MeetingContext.jsx
@@ -9,12 +9,13 @@ export const MeetingProvider = ({ children }) => {
   const [isScreenSharing, setIsScreenSharing] = useState(false);
   const [providerMeetingId, setProviderMeetingId] = useState(null);
   const [showConference, setShowConference] = useState(false);
-
+  const [conferenceId, setConferenceId] = useState(null);
   return (
     <MeetingContext.Provider value={{ 
         conference, setConference, me, setMe, isScreenSharing,
         setIsScreenSharing, providerMeetingId, setProviderMeetingId,
-        showConference, setShowConference}}>
+        showConference, setShowConference,
+    conferenceId, setConferenceId}}>
       {children}
     </MeetingContext.Provider>
   );
