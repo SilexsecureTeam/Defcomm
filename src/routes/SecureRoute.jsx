44d3b4ce0b7c042faaf2c763e9215a971d86b7ab
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-05-28 21:43:11 +0100

    Create SecureRoute.jsx

---
+++ src/routes/SecureRoute.jsx
@@ -0,0 +1,18 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+const SecureRoute = () => {
+  const { authDetails, isLoading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (isLoading) return <div className="text-white text-center mt-10">Loading...</div>;
+
+  if (!authDetails?.user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default SecureRoute;
