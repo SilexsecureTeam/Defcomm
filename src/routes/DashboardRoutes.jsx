9aa030717355acd8cbe32e78a91cb6111b2aab55
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-06-05 17:31:25 +0100

    Update DashboardRoutes.jsx

--- src/routes/DashboardRoutes.jsx
+++ src/routes/DashboardRoutes.jsx
@@ -25,7 +25,7 @@ const Profile = lazy(() => import("../pages/Profile"));
 const Groups = lazy(() => import("../pages/Groups"));
 const SecureChatUI = lazy(() => import("../pages/SecureChatUI"));
 const ShowConferenceRoute = lazy(() => import("../pages/ShowConferenceRoute"));
-
+const WaitingScreen = lazy(() => import("../components/video-sdk/conference/WaitingScreen"));
 const DashboardRoutes = () => {
   const { authDetails } = useContext(AuthContext);
 
@@ -70,6 +70,7 @@ const DashboardRoutes = () => {
             <Route path="isurvive/voice" element={<ChatBoxTwo />} />
             <Route path="settings" element={<Settings />} />
             <Route path="conference" element={<ShowConferenceRoute />} />
+            <Route path="/conference/waiting/:meetingId" element={<WaitingScreen />} />
             <Route path="*" element={<ComingSoon />} />
           </Route>
         </Routes>
