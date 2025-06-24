b4e1679f6ef989e42d9058372953770f5d5089a7
Megabyte-webdev <afolabimubarak18@gmail.com>
2025-06-24 11:34:51 +0100

    new changes

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -71,6 +71,7 @@ const LoginForm = ({ version }) => {
       setTimer(60);
     }
   };
+
   return (
     <div className="w-full max-w-[780px] flex justify-center lg:justify-end items-center pt-20">
       <form
