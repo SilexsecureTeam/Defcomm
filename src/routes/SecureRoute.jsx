aedde8301bd22837e3e62235447d1533419bb647
Megabyte-webdev <afolabimubarak18@gmail.com>
2025-07-30 17:08:46 +0100

    new update

--- src/routes/SecureRoute.jsx
+++ src/routes/SecureRoute.jsx
@@ -9,7 +9,7 @@ const SecureRoute = () => {
   if (isLoading)
     return <div className="text-white text-center mt-10">Loading...</div>;
 
-  if (!authDetails?.user?.status?.toLowerCase() === "active") {
+  if (authDetails?.user?.status?.toLowerCase() !== "active") {
     const isFromLogout = location.state?.fromLogout;
     return (
       <Navigate
